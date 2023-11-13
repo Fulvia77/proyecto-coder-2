9ca135570f696d929035fcf452d21b5dd7aceae6
Flavia D <flavia_andrea@hotmail.com>
2023-11-13 20:13:50 -0300

    prueba

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -14,7 +14,7 @@ $recipient = "info@businessenglishwithflavia.com";
 
 $subject = "Consulta via web de $nombre";
 
-$header = "From: $email \r\n";
+$header = "From: $correo \r\n";
 $header .= "Content-Type: text/plain; charset=UTF-8";
 mail($recipient, $subject, $formcontent, $header) or die("Error!");
 header("Location: gracias.html");
