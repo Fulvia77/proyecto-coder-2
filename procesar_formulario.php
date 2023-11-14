398c90db64f0bb4d143432c4fffdabf14ad64a07
Flavia D <flavia_andrea@hotmail.com>
2023-11-14 20:08:12 -0300

    intento3

--- procesar_formulario.php
+++ procesar_formulario.php
@@ -4,7 +4,7 @@ $nombre = $_POST['nombre'];
 $correo = $_POST['correo'];
 $mensaje = $_POST['mensaje'];
 
-$mensaje = 
+$formcontent = 
     "<html>".
 	    "<head><title>Nuevo Contacto</title></head>".
 	    "<body>".
@@ -12,15 +12,15 @@ $mensaje =
             "<p><strong>Correo:</strong> $correo</p>".
             "<p><strong>Mensaje:</strong> $mensaje</p>".
 	    "</body>".
-	"</html>";
-
+	'</html>';
 
 $recipient = "info@businessenglishwithflavia.com";
 
 $subject = "Consulta via web de $nombre";
 $header = 'MIME-Version: 1.0' . "\r\n";
 $header .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-$header = "From: $recipient \r\n";
+$header .= "From: $recipient \r\n";
+$header .= "Reply-To: ".$correo."\r\n";
 mail($recipient, $subject, $formcontent, $header) or die("Error!");
 header("Location: ./pages/gracias.html");
 
